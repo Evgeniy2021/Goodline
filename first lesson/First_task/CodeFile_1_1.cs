dee29b6ef46922fe15601a13d39229c72027c5fe
Evgeniy2021 <tesel2013@rambler.ru>
2021-03-19 12:38:36 +0700

    Initial commit

---
+++ first lesson/First_task/CodeFile_1_1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class CodeFile_1_1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число месяца");
+            bool i;
+            do
+            {
+                int num = int.Parse(Console.ReadLine());
+                i = true;
+                if (num >= 1 && num <= 12)
+                {
+                    switch (num)
+                    {
+                        case 1: Console.WriteLine("Январь"); break;
+                        case 2: Console.WriteLine("Феврал"); break;
+                        case 3: Console.WriteLine("Март"); break;
+                        case 4: Console.WriteLine("Апрель"); break;
+                        case 5: Console.WriteLine("Май"); break;
+                        case 6: Console.WriteLine("Июнь"); break;
+                        case 7: Console.WriteLine("Июль"); break;
+                        case 8: Console.WriteLine("Август"); break;
+                        case 9: Console.WriteLine("Сентябрь"); break;
+                        case 10: Console.WriteLine("Октябрь"); break;
+                        case 11: Console.WriteLine("Ноябрь"); break;
+                        case 12: Console.WriteLine("Декабрь"); break;
+                    }
+                }
+                else
+                {
+                    i = false;
+                    Console.WriteLine("Вы ошиблись при вводе числа");
+                }
+            } while (i == false);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ first lesson/Second_task/CodeFile_1_2.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Second_task
+{
+    class CodeFile_1_2
+    {
+        static void Main(string[] args)
+        {
+            int x = 0, y = 0;
+            string way;
+            Console.WriteLine("Введите несколько направлени буквами: 'C'- север, 'Ю' -юг, 'В' - восток, 'З' - запад");
+            bool check, letter;
+            do
+            {
+                check = true;
+                letter = true;
+                way = Console.ReadLine().ToLower();
+                if (way.Length > 10)
+                    Console.WriteLine("Можно ввести не более 10 направлений или меньше\nпопробуйте ещё раз");
+                for (int i = 0; i < way.Length; i++)
+                {
+                    if (way[i] >= 'a' && way[i] <= 'z')
+                        check = false;
+                    if (way[i] == 'с' | way[i] == 'ю' | way[i] == 'в' | way[i] == 'з')
+                    {
+                        switch (way[i])
+                        {
+                            case 'с': y++; break;
+                            case 'ю': y--; break;
+                            case 'в': x--; break;
+                            case 'з': x++; break;
+                        }
+                    }
+                    else
+                    {
+                        letter = false;
+                        x = 0;
+                        y = 0;
+                    }
+                }
+                if (check == false)
+                    Console.WriteLine("Используйте русский язык");
+                if (letter == false)
+                    Console.WriteLine("Вы задали неизвестное направление");
+            } while (way.Length > 10 || check == false || letter == false);
+            if (((x == 0) | (y == 0)))
+                Console.WriteLine("Расстояние между точками {0}", x + y);
+            else Console.WriteLine("Расстояние между точками {0}", Math.Round(Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2))));
+            Console.ReadKey();
+        }
+    }
+}
