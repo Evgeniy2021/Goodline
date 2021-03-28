58d57c2fe4e63969d4ad37adcdb6300b1ae36e9a
Evgeniy2021 <tesel2013@rambler.ru>
2021-03-28 22:25:24 +0700

    Updating.

---
+++ second lesson/array/Array.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ArrayNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("укажите размерность массива: ");
+            int arrayLength = int.Parse(Console.ReadLine());
+            int[] mainArray = new int[arrayLength];
+            Console.WriteLine("Введите элементы массива");
+            for (int i = 0; i < arrayLength; i++)
+                mainArray[i] = int.Parse(Console.ReadLine());
+            Array.Sort(mainArray);
+            int count = 1;
+            int selectIndex = mainArray[0];
+            foreach (int iteratioIndexArray in mainArray)
+            {
+                if (iteratioIndexArray != selectIndex)
+                {
+                    count++;
+                    selectIndex = iteratioIndexArray;
+                }
+            }
+            Console.WriteLine("Количество различных элементов: " + count);
+            Console.ReadKey();
+            //Сложность алгоритма: O(n) — линейная сложность
+        }
+    }
+}
---
+++ second lesson/symmetric diagonal/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace symmetric_diagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool symmetric = true;
+            Console.Write("укажите размерность массива: ");
+            int size = int.Parse(Console.ReadLine());
+            int[,] array = new int[size, size];
+            Console.WriteLine("Введите значения");
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    array[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = i + 1; j < array.GetLength(1) - i; j++)
+                {
+                    if (array[i, j] != array[j, i])
+                        symmetric = false;
+                }
+            }
+            if (symmetric == true)
+                Console.WriteLine("Диагональ массива симметрична!");
+            else Console.WriteLine("Диагональ массива не симметрична!");
+            Console.ReadLine();
+            //Сложность алгоритма: O(n2) — квадратичная сложность
+        }
+    }
+}
