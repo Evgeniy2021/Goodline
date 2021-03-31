e00f361f01d71ea284f6c10562fcab3f5c2fb975
Evgeniy2021 <tesel2013@rambler.ru>
2021-03-31 17:01:13 +0700

    Updating.

---
+++ third lesson/postOffice/post_office/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace post_office
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cTaker = 0;
+            Console.WriteLine("Задайте время работы почты (в минутах): ");
+            int postTime = int.Parse(Console.ReadLine());
+            Console.WriteLine("Задайте количество посетителей, желающих забрать посылку: ");
+            int Nvisitors = int.Parse(Console.ReadLine());
+            Console.WriteLine("Задайте время(в минутах)\nзатрачиваемое для обслуживания\nодного посетителя из очереди: ");
+            List<int> time = new();
+            for (int i = 0; i < Nvisitors; i++)
+            {
+                time.Add(int.Parse(Console.ReadLine()));
+                if (postTime > 0)
+                {
+                    postTime -= time[i];
+                    cTaker++;
+                }
+            }
+            time.RemoveRange(0, cTaker);
+            Console.WriteLine("Kоличество посетителей не получивших посылки: {0} ", time.Count);
+        }
+    }
+}
+
+
