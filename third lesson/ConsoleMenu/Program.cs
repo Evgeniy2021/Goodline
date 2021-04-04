abc9bdff4ad4c2361247efee5617207816d1991b
Evgeniy2021 <tesel2013@rambler.ru>
2021-04-04 13:04:34 +0700

    Updating 13:05 04/04/21

---
+++ third lesson/ConsoleMenu/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleMenu
+{
+    class Program
+    {
+        static public string[] MenuStrings =
+           {
+            "1 - Вывод списка",
+            "2 - Добавление элемента",
+            "3 - Поиск элемента по номеру",
+            "4 - Выход"
+        };
+        static void Main(string[] args)
+        {
+
+            Dictionary<string, string> file_format = new()
+            {
+                { "dll", "Dynamic Link Library" },
+                { "dbf", "Database file" },
+                { "mkv", "Matroska Video File" },
+                { "gbr", "Gerber File" },
+                { "txt", "Raw text file" }
+            };
+            //вывод полного списка с обоими значениями
+            foreach (KeyValuePair<string, string> all in file_format)
+            {
+                Console.WriteLine("Формат {0}, это {1}", all.Key, all.Value);
+            }
+
+            //вывод названия по короткому описанию
+            // В случае, если формата не существует, выдайте ошибку и *** перейдите к выбору действия.***
+            Console.WriteLine("\nВведите формат файла для отображения его описания");
+            string format = Console.ReadLine();
+            try
+            {
+                Console.WriteLine("Формат файла \"{1}\" - {0}.", file_format[format], format);
+            }
+            catch (KeyNotFoundException)
+            {
+                Console.WriteLine("Указанного формата \"{0}\" нет в списке.", format);
+                //*** перейдите к выбору действия.***
+            }
+
+            // Добавление нового формата и описания
+            Console.WriteLine("Введите формата файла для его добавления в список");
+            string new_format = Console.ReadLine();
+            Console.WriteLine("Введите описание формата файла для его добавления в список");
+            string description = Console.ReadLine();
+            if (!file_format.ContainsKey(new_format))
+            {
+                file_format.Add(new_format, description);
+                Console.WriteLine("Введённый вами формат \"{0}\" и его описание успешно добавлены в список.",new_format);
+            }
+
+            // Удаление формата и описания
+            Console.WriteLine("Введите формата файла для его удаления из списка");
+            string del_format = Console.ReadLine();
+           // Console.WriteLine("\nУдалить формат \"{0}\" ", Console.ReadLine());
+            file_format.Remove(Console.ReadLine());
+
+            if (!file_format.ContainsKey(del_format))
+            {
+                Console.WriteLine("Key  is not found.");
+            }
+
+        }
+    }
+}
--- third lesson/post_office/Program.cs
+++ third lesson/post_office/Program.cs
@@ -7,24 +7,51 @@ namespace post_office
     {
         static void Main(string[] args)
         {
-            int cTaker = 0;
+            String postTime, Nvisitors, timeVisit;
+            int tempPost_time, temp_visitors, temp_time;
+            bool logpostTime, logNvisitors, logtimeVisit;
             Console.WriteLine("Задайте время работы почты (в минутах): ");
-            int postTime = int.Parse(Console.ReadLine());
-            Console.WriteLine("Задайте количество посетителей, желающих забрать посылку: ");
-            int Nvisitors = int.Parse(Console.ReadLine());
-            Console.WriteLine("Задайте время(в минутах)\nзатрачиваемое для обслуживания\nодного посетителя из очереди: ");
-            List<int> time = new();
-            for (int i = 0; i < Nvisitors; i++)
+            do
             {
-                time.Add(int.Parse(Console.ReadLine()));
-                if (postTime > 0)
+                postTime = Console.ReadLine();
+                logpostTime = Int32.TryParse(postTime, out tempPost_time);
+                if (!logpostTime)
+                    Console.WriteLine("Неверное значение, задайте правильно время работы почты:");
+            } while (!logpostTime);
+            Console.WriteLine("Задайте количество посетителей, желающих забрать посылку:");
+            do
+            {
+                Nvisitors = Console.ReadLine();
+                logNvisitors = Int32.TryParse(Nvisitors, out temp_visitors);
+                if (!logNvisitors)
+                    Console.WriteLine("Ошибка, укажите правильно количество посетителей:");
+                else if (temp_visitors == 0)
+                    Console.WriteLine("Посетителей нет!");
+
+            } while (!logNvisitors);
+            Console.WriteLine("Задайте время(в минутах)\nзатрачиваемое для обслуживания\nодного посетителя из очереди:");
+            do
+            {
+                logtimeVisit = true;
+                Queue<int> time = new();
+                for (int i = 0; i < temp_visitors; i++)
                 {
-                    postTime -= time[i];
-                    cTaker++;
+                    timeVisit = Console.ReadLine();
+                    logtimeVisit = Int32.TryParse(timeVisit, out temp_time);
+                    if (!logtimeVisit)
+                    {
+                        Console.WriteLine("Задайте правильно время : ");
+                        temp_visitors++;
+                    }
+                    time.Enqueue(temp_time);
+                    if (tempPost_time > 0)
+                    {
+                        tempPost_time -= time.Peek();
+                        time.Dequeue();
+                    }
                 }
-            }
-            time.RemoveRange(0, cTaker);
-            Console.WriteLine("Kоличество посетителей не получивших посылки: {0} ", time.Count);
+                Console.WriteLine("Kоличество посетителей не получивших посылки: {0} ", time.Count);
+            } while (!logtimeVisit);
         }
     }
 }
