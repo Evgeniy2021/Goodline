cc4d03f55559eb4b2740fa1ef8bd1c6ea8a81f1b
Evgeniy2021 <tesel2013@rambler.ru>
2021-04-05 13:48:24 +0700

    Updating

--- third lesson/post_office/Program.cs
+++ third lesson/post_office/Program.cs
@@ -7,53 +7,31 @@ namespace post_office
     {
         static void Main(string[] args)
         {
-            String postTime, Nvisitors, timeVisit;
             int tempPost_time, temp_visitors, temp_time;
-            bool logpostTime, logNvisitors, logtimeVisit;
-            Console.WriteLine("Задайте время работы почты (в минутах): ");
-            do
-            {
-                postTime = Console.ReadLine();
-                logpostTime = Int32.TryParse(postTime, out tempPost_time);
-                if (!logpostTime)
-                    Console.WriteLine("Неверное значение, задайте правильно время работы почты:");
-            } while (!logpostTime);
-            Console.WriteLine("Задайте количество посетителей, желающих забрать посылку:");
-            do
-            {
-                Nvisitors = Console.ReadLine();
-                logNvisitors = Int32.TryParse(Nvisitors, out temp_visitors);
-                if (!logNvisitors)
-                    Console.WriteLine("Ошибка, укажите правильно количество посетителей:");
-                else if (temp_visitors == 0)
-                    Console.WriteLine("Посетителей нет!");
 
-            } while (!logNvisitors);
+            Console.WriteLine("Задайте время работы почты (в минутах): ");
+            while (!Int32.TryParse(Console.ReadLine(), out tempPost_time))
+                Console.WriteLine("Неверное значение, задайте правильно время работы почты:");
+
+            Console.WriteLine("Задайте количество посетителей, желающих забрать посылку:");
+            while (!Int32.TryParse(Console.ReadLine(), out temp_visitors))
+                Console.WriteLine("Ошибка, укажите правильно количество посетителей:");
+            if (temp_visitors == 0)
+                Console.WriteLine("Посетителей нет!");
+
             Console.WriteLine("Задайте время(в минутах)\nзатрачиваемое для обслуживания\nодного посетителя из очереди:");
-            do
+            Queue<int> time = new();
+            for (int i = 0; i < temp_visitors; i++)
             {
-                logtimeVisit = true;
-                Queue<int> time = new();
-                for (int i = 0; i < temp_visitors; i++)
+                while (!Int32.TryParse(Console.ReadLine(), out temp_time))
+                    Console.WriteLine("Задайте правильно время : ");
+                time.Enqueue(temp_time);
+                if (tempPost_time > 0)
                 {
-                    timeVisit = Console.ReadLine();
-                    logtimeVisit = Int32.TryParse(timeVisit, out temp_time);
-                    if (!logtimeVisit)
-                    {
-                        Console.WriteLine("Задайте правильно время : ");
-                        temp_visitors++;
-                    }
-                    time.Enqueue(temp_time);
-                    if (tempPost_time > 0)
-                    {
-                        tempPost_time -= time.Peek();
-                        time.Dequeue();
-                    }
+                    tempPost_time -= time.Dequeue();
                 }
-                Console.WriteLine("Kоличество посетителей не получивших посылки: {0} ", time.Count);
-            } while (!logtimeVisit);
+            }
+            Console.WriteLine("Kоличество посетителей не получивших посылки: {0} ", time.Count);
         }
     }
 }
-
-
