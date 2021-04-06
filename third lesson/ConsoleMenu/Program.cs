8c29b11352a42cca4d65501c6782f6b2c70e3f87
Evgeniy2021 <tesel2013@rambler.ru>
2021-04-06 10:22:29 +0700

    Updating

--- third lesson/ConsoleMenu/Program.cs
+++ third lesson/ConsoleMenu/Program.cs
@@ -13,9 +13,107 @@ namespace ConsoleMenu
             "4 - Удаление указанного формата файла",
             "5 - Выход"
         };
+        static public void DisplayMenu()
+        {
+            Console.Clear();
+            foreach (var menuString in MenuStrings)
+            {
+                Console.WriteLine(menuString);
+            }
+            Console.WriteLine("Нажмите цифру, соответствующую номеру меню.");
+        }
+        //DisplayAll(1)
+        static public void DisplayDictionary(Dictionary<string, string> all_content)//вывод полного списка с обоими значениями
+        {
+            Console.Clear();
+            if (all_content.Count == 0)
+            {
+                Console.WriteLine("Список пуст.");
+            }
+            else
+            {
+                foreach (KeyValuePair<string, string> all in all_content)
+                {
+                    Console.WriteLine("Формат {0}, это {1}", all.Key, all.Value);
+                }
+            }
+            Console.WriteLine("Для прехода в меню нажмите любую клавишу...");
+            Console.ReadKey();
+        }
+        //DescriptionTyp(2)
+        static public void DescriptionFile(Dictionary<string, string> typ_file)//вывод названия по короткому описанию
+        {
+            Console.Clear();
+            Console.WriteLine("\nВведите формат файла для отображения его описания");  //1. 
+            string format;
+            do                                                                         //2. 
+            {
+                format = Console.ReadLine();
+                try
+                {
+                    Console.WriteLine("\"{1}\" - {0}.", typ_file[format], format);//формат-описание+
+                    Console.WriteLine("посмотреть другой формат, или для выхода в меню нажмите 'Esc'");
+                }
+                catch (KeyNotFoundException) //перейдите к выбору действия.
+                {
+                    Console.WriteLine("Указанного формата \"{0}\" нет в списке.", format);
+                    Console.WriteLine("Попробутй ещё раз, или для выхода в меню нажмите 'Esc'");
+
+                }
+            } while (typ_file.ContainsKey(format) | Console.ReadKey().Key != ConsoleKey.Escape);
+            Console.WriteLine("\nДля прехода в меню нажмите любую клавишу...");
+            Console.ReadKey();
+        }
+        //AttachTyp(3)
+        static public void AttachTypFile(Dictionary<string, string> attach_typ)// Добавление нового формата и описания
+        {
+            Console.Clear();
+            Console.WriteLine("Введите формата файла для его добавления в список");
+            string new_format = Console.ReadLine();
+            while (new_format.Trim() == "")
+            {
+                Console.Write("Попробуйте снова ввести формат файла: ");
+                new_format = Console.ReadLine();
+            }
+            Console.WriteLine("Введите описание формата файла для его добавления в список");
+            string description = Console.ReadLine();
+            while (description.Trim() == "")
+            {
+                Console.Write("Попробуйте снова ввести имя: ");
+                description = Console.ReadLine();
+            }
+            if (!attach_typ.ContainsKey(new_format))
+            {
+                attach_typ.Add(new_format, description);
+                Console.WriteLine("Введённый вами формат \"{0}\" и его описание успешно добавлены в список.", new_format);
+            }
+            Console.WriteLine("Для прехода в меню нажмите любую клавишу...");
+            Console.ReadKey();
+        }
+        //RemoveTyp(4)
+        static public void RemoveTypFile(Dictionary<string, string> remove_typ)// Удаление формата и описания
+        {
+            Console.Clear();
+            Console.WriteLine("Введите формата файла для его удаления из списка");
+            string del_format = Console.ReadLine();
+            while (del_format.Trim() == "")
+            {
+                Console.Write("Попробуйте снова ввести формат: ");
+                del_format = Console.ReadLine();
+            }
+            // Console.WriteLine("\nУдалить формат \"{0}\" ", Console.ReadLine());
+            if (!remove_typ.ContainsKey(del_format))
+            {
+                Console.WriteLine("Формат файла не найден.");
+            }
+            else
+            {
+                remove_typ.Remove(del_format);
+                Console.WriteLine("Формат файла и его описание удалены.");
+            }
+        }
         static void Main(string[] args)
         {
-
             Dictionary<string, string> file_format = new()
             {
                 { "dll", "Dynamic Link Library" },
@@ -24,48 +122,30 @@ namespace ConsoleMenu
                 { "gbr", "Gerber File" },
                 { "txt", "Raw text file" }
             };
-            //вывод полного списка с обоими значениями
-            foreach (KeyValuePair<string, string> all in file_format)
+            ConsoleKey key = ConsoleKey.Enter;
+            do
             {
-                Console.WriteLine("Формат {0}, это {1}", all.Key, all.Value);
-            }
-
-            //вывод названия по короткому описанию
-            // В случае, если формата не существует, выдайте ошибку и *** перейдите к выбору действия.***
-            Console.WriteLine("\nВведите формат файла для отображения его описания");
-            string format = Console.ReadLine();
-            try
-            {
-                Console.WriteLine("Формат файла \"{1}\" - {0}.", file_format[format], format);
-            }
-            catch (KeyNotFoundException)
-            {
-                Console.WriteLine("Указанного формата \"{0}\" нет в списке.", format);
-                //*** перейдите к выбору действия.***
-            }
-
-            // Добавление нового формата и описания
-            Console.WriteLine("Введите формата файла для его добавления в список");
-            string new_format = Console.ReadLine();
-            Console.WriteLine("Введите описание формата файла для его добавления в список");
-            string description = Console.ReadLine();
-            if (!file_format.ContainsKey(new_format))
-            {
-                file_format.Add(new_format, description);
-                Console.WriteLine("Введённый вами формат \"{0}\" и его описание успешно добавлены в список.",new_format);
-            }
-
-            // Удаление формата и описания
-            Console.WriteLine("Введите формата файла для его удаления из списка");
-            string del_format = Console.ReadLine();
-           // Console.WriteLine("\nУдалить формат \"{0}\" ", Console.ReadLine());
-            file_format.Remove(Console.ReadLine());
-
-            if (!file_format.ContainsKey(del_format))
-            {
-                Console.WriteLine("Key  is not found.");
-            }
-
+                DisplayMenu();
+                key = Console.ReadKey().Key;
+                switch (key)
+                {
+                    case ConsoleKey.D1:
+                        DisplayDictionary(file_format);
+                        break;
+                    case ConsoleKey.D2:
+                        DescriptionFile(file_format);
+                        break;
+                    case ConsoleKey.D3:
+                        AttachTypFile(file_format);
+                        break;
+                    case ConsoleKey.D4:
+                        RemoveTypFile(file_format);
+                        break;
+                    default: continue;
+                }
+            } while (key != ConsoleKey.D5);
+            Console.Clear();
+            Console.WriteLine("Пока!");
         }
     }
 }
