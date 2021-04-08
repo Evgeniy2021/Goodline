823b998c864099b75c98da8e6c4d6559e0e4de34
Evgeniy2021 <tesel2013@rambler.ru>
2021-04-09 01:16:56 +0700

    Updating

---
+++ fourth lesson/DistinctCount_item4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DistinctCount_item4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbers, num;
+            Console.WriteLine("Задайте общее колличество значений: ");
+            while (!Int32.TryParse(Console.ReadLine(), out numbers))
+                Console.WriteLine("Задайте целое число!");
+            Console.WriteLine("\nВведите значения:");
+            List<int> listNumb = new(numbers);
+            for (int i = 0; i < numbers; i++)
+            {
+                while (!Int32.TryParse(Console.ReadLine(), out num))
+                    Console.WriteLine("Введите целое число: ");
+                listNumb.Add(num);
+            }
+            // Count<TSource>(IEnumerable<TSource>, Func<TSource, Boolean>)
+            int disCount = Enumerable.Count<int>(listNumb.Distinct());
+            Console.WriteLine("\nКолличество разных чисел: {0}", disCount);
+            Console.ReadKey();
+        }
+    }
+}
