b6dcc0788930fa8e9a707b8a8e9958adc6e87ac0
Evgeniy2021 <tesel2013@rambler.ru>
2021-04-13 11:49:13 +0700

    Updating

---
+++ fourth lesson/RandomClass_item5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RandomClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random value = new Random();
+            List<int> chaos = new();
+            for (int i = 0; i < 50; i++)
+            {
+                chaos.Add(value.Next(0, 26));
+                /*foreach (int RandVal in chaos)
+                {
+                    Console.Write(RandVal + " ");
+                }*/
+            }
+            //Console.WriteLine("\n");
+            IEnumerable<int> temp_val = chaos.Where(num => num >= 10 && num <= 20).Distinct();
+            var sortVal = temp_val.OrderByDescending(val => val);
+            foreach (int upshot in sortVal)
+            {
+                Console.Write(upshot + " ");
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
