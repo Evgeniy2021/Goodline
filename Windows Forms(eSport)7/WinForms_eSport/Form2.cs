96742f88b367cd572defdddaedfb266d4c6f161a
Evgeniy2021 <tesel2013@rambler.ru>
2021-05-25 19:48:47 +0700

    Updating

--- Windows Forms(eSport)7/WinForms_eSport/Form2.cs
+++ Windows Forms(eSport)7/WinForms_eSport/Form2.cs
@@ -10,8 +10,9 @@ namespace WinForms_eSport
         }
         private void radioButton1_CheckedChanged(object sender, System.EventArgs e)
         {
-            Form1 newForm = new Form1();
-            newForm.Show();
+           // Form1 newForm = new Form1();
+           // newForm.Show();
+
             this.Close();
             //Application.ExitThread();
 
--- Windows Forms(eSport)7/WinForms_eSport/Program.cs
+++ Windows Forms(eSport)7/WinForms_eSport/Program.cs
@@ -85,7 +85,8 @@ namespace WinForms_eSport
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form2());
-           // Application.Run(new Form1());
+            Application.Run(new Form1());
+
 
         }
     }
